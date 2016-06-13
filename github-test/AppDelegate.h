1843229dd34ee4c584ca0ebebe0c8552788c4212
wangshangdongliwuwei <ncuwuwei@163.com>
2016-06-13 14:05:32 +0800

    Initial Commit

---
+++ github-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github-test
+//
+//  Created by 网商 on 16/6/13.
+//  Copyright © 2016年 网商. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github-test
+//
+//  Created by 网商 on 16/6/13.
+//  Copyright © 2016年 网商. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
